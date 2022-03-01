2dc011e43be5a13244858e2094035fa268e5bc36
Mahnoor Khan <68016846+mahnoork18@users.noreply.github.com>
2022-03-01 15:10:19 +0500

    update advice id

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
 //Get advcie id
 //getadvice text
 let adviceText = document.querySelector('#advice');
-let adviceNum = document.querySelector('#id');
 let adviceBtn = document.querySelector('#cta');
 
+adviceBtn.addEventListener('submit', () =>{
+    getAdvice();
+});
 function getAdvice(){
     fetch('https://api.adviceslip.com/advice').then(response =>{
         return response.json();
     }).then(adviceData => {
-        const adviceId = adviceData.slip;
-        adviceText.innerHTML = adviceId.advice;
+        const advice_Text = adviceData.slip;
+        adviceText.innerHTML = advice_Text.advice;
         // const adviceNum = adviceData.slip[0];
         // adviceNum.innerHTML = adviceId.slip;
+        const advice_Id = document.querySelector("#advice_id");
+const advice = adviceData.slip;
+advice_Id.innerHTML = `Advice #${advice.id}`;
         
 
     }).catch(err => {
         adviceText.innerHTML = "Error";
     })
-}
-
-window.onload = () =>{
-    getAdvice();
 };
-adviceBtn.addEventListener('click', () =>{
-    getAdvice();
-});
 
-   
